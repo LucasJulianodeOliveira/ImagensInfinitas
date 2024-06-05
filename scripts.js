f0abb78a4f299df4851206eebf8cf5af5ff6c9a2
Seiyato <165904877+LucasJulianodeOliveira@users.noreply.github.com>
2024-06-05 13:55:34 -0300

    Inicial

---
+++ scripts.js
@@ -0,0 +1,29 @@
+let item = 0;
+const max = 9;
+
+function proxImagem( img ){
+    fetch(`img/${img}.jpg`)
+    .then(resp => resp.blob())
+    .then(blob => {
+        const imageObjectURL = URL.createObjectURL(blob);
+        console.log(imageObjectURL);
+
+        const proxImg = document.createElement("img");
+        proxImg.src = imageObjectURL;
+        document.getElementById("placeholder").appendChild(proxImg);
+    })
+}
+window.onload = function(){
+    for (; item < 6; item++){
+        proxImagem(item);
+    }
+}
+
+window.onscroll = function(){
+    let altura = document.body.scrollHeight;
+    let scrollPoint = window.scrollY + window.innerHeight;
+    if (scrollPoint >= altura){
+        proxImagem(item++ % max)
+    }
+}
+
